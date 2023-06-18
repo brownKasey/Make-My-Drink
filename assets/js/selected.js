182b4c0d9c252a929eb4bc4660225c931b167a58
Monika <raciam@oregonstate.edu>
2023-06-18 12:20:51 -0500

    Add function for filtering by spirit type; update layout and styling

--- assets/js/selected.js
+++ assets/js/selected.js
@@ -2,10 +2,12 @@ var selectedImage = document.querySelector('#selected-image');
 var ingredientBox = document.querySelector('#ingredient-box');
 var stepBox = document.querySelector('#step-box');
 var drinkId = JSON.parse(localStorage.getItem('drinkID'));
+var goBackButton = document.querySelector('#go-back-button');
 
 function getCocktailResults() {
     drinkId = JSON.parse(localStorage.getItem('drinkID'));
     var requestUrl = 'https://thecocktaildb.com/api/json/v2/9973533/lookup.php?i=' + drinkId;
+
     //console.log(requestUrl);
     fetch(requestUrl)
     .then(function (response) {
@@ -61,7 +63,6 @@ function displayCocktails(data) {
         var ingredient = measuresArray[i] + ' ' + ingredientsArray[i];
         bigString = bigString + ' ' + ingredient + ',';
         //console.log('Testing ingredient ' + i + ': ' + ingredient);
-        //fetchFunction(bigString);
     }
 
     // Print each step in list format
@@ -78,5 +79,12 @@ function displayCocktails(data) {
         }
     }
 }
+
+function goBack() {
+    //console.log("Testing go back button");
+    window.location.href='./index.html'
+}
+
+goBackButton.addEventListener('click', goBack);
 
 getCocktailResults();
